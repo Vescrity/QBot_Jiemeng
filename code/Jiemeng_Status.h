a96585ec70d024acef58c84ae5fb2466ae30111d
Vescrity <vescrity@foxmail.com>
2024-02-08 20:07:51 +0900

    修改：     README.md

--- code/Jiemeng_Status.h
+++ code/Jiemeng_Status.h
@@ -3,17 +3,20 @@
 #include "Jiemeng_Ans.h"
 #include <iostream>
 #include <sstream>
+#include "Time_Struct.h"
 using namespace std;
+
+str_tm start_time;
+
 string jm_status()
 {
   int ans_size = answers.ans_group.size();
   std::ostringstream oss;
-
-  std::string text = "Hello, world!";
-
+  oss << "启动时间：" << start_time.year << "-" << start_time.month << "-" << start_time.mday
+      << " " << start_time.hour << ":" << start_time.min << ":" << start_time.sec << endl;
   oss << "已加载词库: " << ans_size << "组" << endl;
 #ifdef JIEMENG_DECK
-  oss << "可用牌堆词条数: " << decks.size() << endl;
+  oss << "可用牌堆词条数: " << decks.size();
 #endif
   return oss.str();
 }
