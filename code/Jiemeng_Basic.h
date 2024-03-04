fe05a98e858e46889b368af229866af9a64fc901
Vescrity <vescrity@foxmail.com>
2024-03-04 22:54:10 +0900

    Debug Colour等

--- code/Jiemeng_Basic.h
+++ code/Jiemeng_Basic.h
@@ -4,7 +4,7 @@
 #define LINUX_V
 #endif
 
-#define JIEMENG_VERSION ("v24.11.15")
+#define JIEMENG_VERSION ("v24.11.16")
 #ifdef _WIN32
 #define JIEMENG_PLATFORM "Jiemeng_Windows (Limited Feature)"
 #else
@@ -23,6 +23,7 @@
 #include "r_adb.h"
 #include "Jiemeng_Exception.h"
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 bool Debug_Mode = 0;
 
@@ -46,6 +47,8 @@ inline void print_time_mark()
   sec = tmp_ptr->tm_sec;
   printf("\033[0m\033[0;37m[%02d:%02d:%02d]", hr, mn, sec);
 }
+/// @brief CYAN
+/// @param s
 inline void msg_lable(const char *s)
 {
   print_time_mark();
@@ -59,22 +62,66 @@ inline void msg_puts(const char *s)
 {
   printf("\033[0m\033[1;36m%s\n\033[0m", s);
 }
+/// @brief MAGENTA
+/// @param s
 inline void debug_print(const char *s)
 {
   if (Debug_Mode)
-    printf("\033[0m\033[7;32m%s\033[0m", s);
+    printf("\033[0m\033[35m%s\033[0m", s);
 }
 inline void debug_puts(const char *s)
 {
   if (Debug_Mode)
-    printf("\033[0m\033[7;32m%s\n\033[0m", s);
+    printf("\033[0m\033[35m%s\n\033[0m", s);
 }
+inline void debug_lable(const char *s)
+{
+  if (Debug_Mode)
+  {
+    print_time_mark();
+    printf("\033[0m\033[7;35m%s\033[0m", s);
+  }
+}
+
+class DoutStream
+{
+public:
+  template <typename T>
+  DoutStream &operator<<(const T &value)
+  {
+    dprint(std::to_string(value).c_str());
+    return *this;
+  }
+  DoutStream &operator<<(const char *value)
+  {
+    dprint(value);
+    return *this;
+  }
+  DoutStream &operator<<(const string &value)
+  {
+    dprint(value.c_str());
+    return *this;
+  }
+  DoutStream &operator<<(const nlohmann::json &json)
+  {
+    dprint(json.dump().c_str());
+    return *this;
+  }
+
+private:
+  void dprint(const char *message)
+  {
+    debug_print(message);
+  }
+} dout;
 inline void func_show(const char *s)
 {
 #ifdef _FUNC_SHOW_
   debug_puts(s);
 #endif
 }
+/// @brief GREEN
+/// @param s
 inline void info_lable(const char *s)
 {
   print_time_mark();
@@ -88,6 +135,9 @@ inline void info_puts(const char *s)
 {
   printf("\033[0m\033[1;32m%s\n\033[0m", s);
 }
+
+/// @brief BLUE
+/// @param s
 inline void color_lable(const char *s)
 {
   print_time_mark();
@@ -101,6 +151,23 @@ inline void color_print(const char *s)
 {
   printf("\033[0m\033[1;34m%s\033[0m", s);
 }
+/// @brief YELLOW
+/// @param s
+inline void warn_lable(const char *s)
+{
+  print_time_mark();
+  printf("\033[0m\033[1;7;33m%s\033[0m", s);
+}
+inline void warn_puts(const char *s)
+{
+  printf("\033[0m\033[1;33m%s\n\033[0m", s);
+}
+inline void warn_print(const char *s)
+{
+  printf("\033[0m\033[1;33m%s\033[0m", s);
+}
+/// @brief RED
+/// @param s
 inline void error_lable(const char *s)
 {
   print_time_mark();
@@ -137,7 +204,6 @@ string execmd(const string &commd)
 #ifdef _WIN32
   if (hasNonUTF8(output))
   {
-
     string str = GBKToUTF8(output.c_str());
     return str;
   }
