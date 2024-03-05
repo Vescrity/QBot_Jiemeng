eb3cb91708e2342987e154a809909c5421260e70
Vescrity <vescrity@foxmail.com>
2024-03-05 21:37:21 +0900

    修改：     code/Weather_Report.h

--- code/Jiemeng_Basic.h
+++ code/Jiemeng_Basic.h
@@ -4,7 +4,7 @@
 #define LINUX_V
 #endif
 
-#define JIEMENG_VERSION ("v24.11.17")
+#define JIEMENG_VERSION ("v24.11.18")
 #ifdef _WIN32
 #define JIEMENG_PLATFORM "Jiemeng_Windows (Limited Feature)"
 #else
