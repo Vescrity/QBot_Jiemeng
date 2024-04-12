aa2841a5f62b13b73fa805301de528f194349ccf
Vescrity <vescrity@foxmail.com>
2024-04-12 22:56:31 +0800

    sleep & order

--- code/Jiemeng_Operation.hpp
+++ code/Jiemeng_Operation.hpp
@@ -20,6 +20,7 @@ public:
     lua_once,
     call_state,
     ignore,
+    sleep,
     draw_deck,
     clear
   } type;
@@ -64,20 +65,5 @@ public:
     return result;
   }
 };
-/*
 
-- Operator_List get_oper  : 传入消息，经过应答队列处理返回操作列
-    原始操作列：
-      1. 词库应答词条，未经转化处理
-      2. 指令操作：指令名与参数列
-        a. 不携带json的指令使用 lua 函数调用
-
-
-
-    处理操作列：
-      1. 全部转换为输出操作、睡眠操作、变量操作
-
-
-
-*/
 #endif
