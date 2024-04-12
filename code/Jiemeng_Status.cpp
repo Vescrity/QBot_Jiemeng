2ad9c0f3715231b8d0540b25ec7521381180a5c8
Vescrity <vescrity@foxmail.com>
2024-04-12 22:55:48 +0800

    Status

---
+++ code/Jiemeng_Status.cpp
@@ -0,0 +1,16 @@
+#include "Jiemeng_Time.hpp"
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+Time_Class start_time;
+string start_up_time()
+{
+  char times[100];
+  sprintf(times, "%04d-%02d-%02d %02d:%02d:%02d",
+          start_time.nyear, start_time.nmonth,
+          start_time.nmday, start_time.nhour,
+          start_time.nmin, start_time.nsec);
+  return times;
+}
