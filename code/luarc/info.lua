a678b7f00111a0b16c48be53cc8cc19d3b891827
Vescrity <vescrity@foxmail.com>
2024-04-12 23:56:43 +0800

    info

--- code/luarc/info.lua
+++ code/luarc/info.lua
@@ -1,3 +1,7 @@
 function bot.info()
-  return bot._platform..' '..bot._version..'\nCompiled at '..bot._compile_time..'\nLua version: '.._VERSION
+    return bot._platform .. ' ' .. bot._version .. 
+    '\nCompiled at ' ..               bot._compile_time .. 
+    '\nLua version: ' .. _VERSION ..
+    '\nMore information at https://github.com/Vescrity/QBot_Jiemeng'..
+    '\nDo not use me too hard♡'
 end
