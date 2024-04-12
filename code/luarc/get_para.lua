d21a7d68b87c1f896499dad5bf3bcd3198095fde
Vescrity <vescrity@foxmail.com>
2024-04-12 22:56:49 +0800

    完善注释

--- code/luarc/get_para.lua
+++ code/luarc/get_para.lua
@@ -1,6 +1,6 @@
--- input: 'abc#def' '#'
--- return: 'def' 'abc'
 function get_para(s, spliter)
+    -- input: 'abc#def' '#'
+    -- return: 'def' 'abc'
     spliter = spliter or bot.spliter
     local pos = s:find(spliter)
     if pos then
