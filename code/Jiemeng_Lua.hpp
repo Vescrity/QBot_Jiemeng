f6eac3d3f8c1540cce992b5b9e59d0281339a182
Vescrity <vescrity@foxmail.com>
2024-04-13 12:16:01 +0800

    lua_reload

--- code/Jiemeng_Lua.hpp
+++ code/Jiemeng_Lua.hpp
@@ -5,15 +5,25 @@
 #include <sol/sol.hpp>
 #include <mutex>
 #include <condition_variable>
+#include <memory>
 class Jiemeng;
 class Lua_Shell
 {
-  sol::state lua;
+  //sol::state &lua;
+  std::unique_ptr<sol::state>lua;
   Jiemeng *bot;
   void init(Jiemeng *b);
   std::mutex mtx;
+
 public:
-  Lua_Shell(Jiemeng *b) { init(b); };
+  Lua_Shell(Jiemeng *b)
+  {
+    lua = std::make_unique<sol::state>();
+    bot = nullptr;
+    //lua = *lp;
+    init(b);
+  };
+  void reload();
   /// @brief 调用一个 Lua 函数，并将 Message 转为 Lua table 传递
   /// @param func_name 函数名
   /// @param  msg 需要传递的消息
