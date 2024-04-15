7a928779eaec7adf707cc795ae0f0bba5ecd8c10
Vescrity <vescrity@foxmail.com>
2024-04-15 16:10:19 +0800

    fix reRecv, Add try-catch

--- code/Jiemeng_Output.cpp
+++ code/Jiemeng_Output.cpp
@@ -2,29 +2,45 @@
 #include "txt2img_api.hpp"
 bool Jiemeng::private_output(const string &user_id, CQMessage &message)
 {
-  if (message.str().length() == 0 || user_id == "10000")
+  try
+  {
+    if (message.str().length() == 0 || user_id == "10000")
+      return false;
+    blue_lable("[Output]");
+    blue_puts(message.const_str());
+    json send_data, rt;
+    send_data["action"] = "send_private_msg";
+    send_data["params"]["user_id"] = stoull(user_id);
+    send_data["params"]["message"] = message.get_json();
+    rt = ws_send(send_data);
+    return rt["status"] == "ok";
+  }
+  catch (exception &e)
+  {
+    JM_EXCEPTION("[Output]")
     return false;
-  blue_lable("[Output]");
-  blue_puts(message.const_str());
-  json send_data, rt;
-  send_data["action"] = "send_private_msg";
-  send_data["params"]["user_id"] = stoull(user_id);
-  send_data["params"]["message"] = message.get_json();
-  rt = ws_send(send_data);
-  return rt["status"] == "ok";
+  }
 }
 bool Jiemeng::group_output(const string &group_id, CQMessage &message)
 {
-  if (message.str().length() == 0)
+  try
+  {
+    if (message.str().length() == 0)
+      return false;
+    blue_lable("[Output]");
+    blue_puts(message.const_str());
+    json send_data, rt;
+    send_data["action"] = "send_group_msg";
+    send_data["params"]["group_id"] = stoull(group_id);
+    send_data["params"]["message"] = message.get_json();
+    rt = ws_send(send_data);
+    return rt["status"] == "ok";
+  }
+  catch (exception &e)
+  {
+    JM_EXCEPTION("[Output]")
     return false;
-  blue_lable("[Output]");
-  blue_puts(message.const_str());
-  json send_data, rt;
-  send_data["action"] = "send_group_msg";
-  send_data["params"]["group_id"] = stoull(group_id);
-  send_data["params"]["message"] = message.get_json();
-  rt = ws_send(send_data);
-  return rt["status"] == "ok";
+  }
 }
 bool Jiemeng::message_output(const Message_Place &place, CQMessage &message)
 {
