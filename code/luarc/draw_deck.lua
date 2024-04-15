48a343be6490bbd8208921458d636357cedfeb49
Vescrity <vescrity@foxmail.com>
2024-04-15 14:10:19 +0800

    lua 多次deck抽取

--- code/luarc/draw_deck.lua
+++ code/luarc/draw_deck.lua
@@ -1,11 +1,17 @@
-function bot.basic_draw_deck(key, message)
-    local raw = bot._draw_deck(key)
-    return bot.message_replace(raw, message.place)
+function bot.basic_draw_deck(message, key, times)
+    times = times or 1
+    rt = ''
+    for i = 1, times do
+        local raw = bot._draw_deck(key)
+        rt = rt .. bot.message_replace(raw, message.place) .. '\n'
+    end
+    return rt
 end
 
 function bot.draw_deck(message)
     --  @param Message message
     --  @return string 抽取结果
     local key = get_para(message:true_str())
-    return bot.basic_draw_deck(key, message)
+    local times, keys = get_para(key)
+    return bot.basic_draw_deck(message, keys, times)
 end
