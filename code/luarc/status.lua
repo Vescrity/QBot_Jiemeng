72bd031ac930629d42c7c0c8d2123a916d9527ea
Vescrity <vescrity@foxmail.com>
2024-04-15 14:10:48 +0800

    status 追加项目

--- code/luarc/status.lua
+++ code/luarc/status.lua
@@ -1,7 +1,11 @@
 function bot.status()
     local rt = bot._platform .. ' ' .. bot._version .. '\n'
-    rt = rt..'启动时间: '..bot.start_up_time()..'\n'
-    rt = rt..'已加载牌堆: '..bot.deck_size()..'\n'
-    rt = rt..'已加载词库: '..bot.answer_size()..'\n'
-    return rt
+    rt = rt .. '启动时间: ' .. bot.start_up_time() .. '\n'
+    rt = rt .. '已加载牌堆词条: ' .. bot.deck_size() .. '\n'
+    rt = rt .. '已加载应答组: ' .. bot.answer_size() .. '\n'
+    rt = rt .. '参数分隔符：' .. "'" .. bot.spliter .. "'" .. '\n'
+    rt = rt .. '前端信息: \n'
+    local qwq = {action = 'get_version_info', params = {}}
+    s = bot.ws_send(qwq).data:dump(2)
+    return rt .. s
 end
