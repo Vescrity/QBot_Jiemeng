df64b7de59a5dcf3613196c92534722ffd8d4644
vescrity <vescrity@foxmail.com>
2024-12-03 21:00:30 +0800

    something not important

--- code/luarc/os_sh.lua
+++ code/luarc/os_sh.lua
@@ -1,5 +1,5 @@
 function bot.os_sh(cmd)
-    local command = cmd .. ' 2>&1'
+    local command = 'COLUMNS=70 ' .. cmd .. ' 2>&1'
     local handle = io.popen(command)
     local result = handle:read("*a") 
     handle:close()
