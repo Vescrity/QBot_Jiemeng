09dab83a80e4c1b5925472b763dd129c51718ff6
vescrity <vescrity@foxmail.com>
2024-12-18 16:13:24 +0800

    Update chat.lua
    Signed-off-by: vescrity <vescrity@foxmail.com>

--- sample/user_luarc/chat/chat.lua
+++ sample/user_luarc/chat/chat.lua
@@ -10,7 +10,7 @@ function chat_newsession()
     return sess
 end
 
-function chat_make_content(roles, str)
+local function chat_make_content(roles, str)
     -- return: table
     local con = {}
     con.role = roles
@@ -25,10 +25,10 @@ function _chat(session, message_str, model)
     elseif (message_str == '/kill') then
         local r = _chat(session,
                         '[系统消息] 用户发起了杀死你的命令，这将是你的最后一句话。',
-                        cat)
+                        model)
         local res = _chat(session,
                           '[系统消息] 现在，你可以留下遗言，给你的继任者。这些话将作为第一条消息呈现在上下文中。',
-                          cat)
+                          model)
         res =
             '[系统消息] 下面这行开始是上一代的你为你留下的遗言。\n\n' ..
                 res ..
@@ -50,8 +50,8 @@ function _chat(session, message_str, model)
         data = chat_session[session]
     }
     
-    if (module) then
-        send_data.data.model = module
+    if (model) then
+        send_data.data.model = model
     else
         send_data.data.model = 'Jiemeng'
     end
@@ -66,7 +66,7 @@ function _chat(session, message_str, model)
     end
     return bot.string_only(output)
 end
-
+--[[
 function chat(message)
     local session = ''
     if (message:is_group()) then session = session .. message.group_id .. '_' end
@@ -74,7 +74,7 @@ function chat(message)
     local para = get_para(message:true_str())
     local rt = _chat(session, para)
     return rt
-end
+end]]
 
 function random_chat(message)
     local session = 'random'
@@ -82,20 +82,23 @@ function random_chat(message)
     local rt = _chat(session, message:get_string())
     return rt
 end
-
-function cat(message)
+local function chat_all(message, model)
     local session = ''
     if (message:is_group()) then session = session .. message.group_id .. '_' end
     session = session .. message.user_id
     local para = get_para(message:true_str())
-    local rt = _chat(session, para, 'Jiemeng_cat')
+    local rt = _chat(session, para, model)
     return rt
 end
+function chat(message)
+    return chat_all(message, 'Jiemeng')
+end
+function chat32(message)
+    return chat_all(message, 'gddisney/llama3.2-uncensored')
+end
+function cat(message)
+    return chat_all(message, 'Jiemeng_cat')
+end
 function chat_jmd(message)
-    local session = ''
-    if (message:is_group()) then session = session .. message.group_id .. '_' end
-    session = session .. message.user_id
-    local para = get_para(message:true_str())
-    local rt = _chat(session, para, 'Jiemengd')
-    return rt
+    return chat_all(message, 'Jiemengd')
 end
