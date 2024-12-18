595cb9fac6770fb0e40d992d84c94345ae43f5a6
vescrity <vescrity@foxmail.com>
2024-12-18 16:11:09 +0800

    move del_msg to onebot-api.lua
    Signed-off-by: vescrity <vescrity@foxmail.com>

--- code/luarc/del_msg.lua
+++
@@ -1,8 +0,0 @@
----
----@param msg_id integer
----@return 
-function bot.del_msg(msg_id)
-    local dt = {action = 'delete_msg', params = {message_id = msg_id}}
-    bot._ws_send(dt)
-    return ''
-end
