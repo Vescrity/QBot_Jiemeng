2e358e9266ead38e6a48f7f79589ae7534f542b3
vescrity <vescrity@foxmail.com>
2024-12-30 20:28:42 +0800

    [Lua/deck] make it clearer

--- code/luarc/bot_deck.lua
+++ code/luarc/bot_deck.lua
@@ -5,15 +5,13 @@ DECK_MAX = DECK_MAX or 20
 ---
 ---@param message Message
 ---@param key string
----@param times string | integer | nil
+---@param times integer
 ---@return string
 function bot.basic_draw_deck(message, key, times)
-    times = times or 1
-    times = tonumber(times)
     local rt = ''
     if (times==nil or times > DECK_MAX or times <= 0) then return '无效的参数值。' end
     for i = 1, times do
-        local raw = bot._draw_deck(key)
+        local raw = bot.draw_deck(key)
         rt = rt .. bot.message_replace(raw, message) .. '\n'
     end
     return rt:gsub("%s+$","")
@@ -24,5 +22,11 @@ end
 function bot.mapi.draw_deck(message)
     local key = reverse_split(message:true_str())
     local times, keys = reverse_split(key)
-    return bot.basic_draw_deck(message, keys, times)
+    local t = 0
+    if times == '' then
+        t = 1
+    else
+        t = tonumber(times) // 1
+    end
+    return bot.basic_draw_deck(message, keys, t)
 end
