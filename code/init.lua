2c7e7ce529a01719abbd48c940d054dfc12dd0c4
vescrity <vescrity@foxmail.com>
2024-12-30 20:28:03 +0800

    [Lua] update init

--- code/init.lua
+++ code/init.lua
@@ -1,2 +1,6 @@
 package.cpath = package.cpath .. ";./luaclib/?.so"
+package.path = package.path .. ";./luarc/?.lua"
 package.path = package.path .. ";./lualib/?.lua"
+DECK_MAX = 20
+require("bot_meta")
+require("txt2img")
