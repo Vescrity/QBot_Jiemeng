4cc3fb090fd4ea7b77279cae8949cd025993273a
vescrity <vescrity@foxmail.com>
2024-12-30 10:27:05 +0800

    [lua/info] Fix.

--- code/luarc/bot_info.lua
+++ code/luarc/bot_info.lua
@@ -1,5 +1,5 @@
 
----comment
+---返回 bot 版本信息
 ---@return string
 function bot.info()
     return bot._platform .. ' ' .. bot._version ..
@@ -8,13 +8,13 @@ function bot.info()
     '\nMore information at https://github.com/Vescrity/QBot_Jiemeng'..
     '\nDo not use me too hard♡'
 end
----comment
----@return unknown
+---返回 bot 状态信息
+---@return string
 function bot.status()
     local rt = bot._platform .. ' ' .. bot._version .. '\n'
     rt = rt .. '启动时间: ' .. bot.start_up_time() .. '\n'
-    rt = rt .. '已加载牌堆词条: ' .. bot.deck_size() .. '\n'
-    rt = rt .. '已加载应答组: ' .. bot.answer_size() .. '\n'
+    rt = rt .. '已加载牌堆词条: ' .. bot.get_deck_size() .. '\n'
+    rt = rt .. '已加载应答组: ' .. bot.get_answer_size() .. '\n'
     rt = rt .. '参数分隔符：' .. "'" .. bot.spliter .. "'" .. '\n'
     rt = rt .. '前端信息: \n'
     local qwq = {action = 'get_version_info', params = {}}
