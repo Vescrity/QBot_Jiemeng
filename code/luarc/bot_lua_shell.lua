688f50da9cdb6e1ee303050aaa2e1ee2f416f695
vescrity <vescrity@foxmail.com>
2025-08-15 18:16:53 +0800

    [Lua] update

--- code/luarc/bot_lua_shell.lua
+++ code/luarc/bot_lua_shell.lua
@@ -1,8 +1,8 @@
 require('bot_string')
 local reverse_split = bot.string.reverse_split
 ---comment
----@param script any
----@return any
+---@param script string
+---@return string
 function bot.lua_shell_explain(script)
     local scriptToExecute = reverse_split(script)
     if scriptToExecute then
@@ -18,9 +18,7 @@ function bot.lua_shell_explain(script)
 end
 ---comment
 ---@param order Message
----@return any
+---@return string
 function bot.lua_shell(order)
-    --  @param Message order 携带 Lua 代码
-    --  @return string 运行结果
     return bot.lua_shell_explain(order:true_str())
 end
