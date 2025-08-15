5b331d1e874a5ccd920eaec152b013fb66420075
vescrity <vescrity@foxmail.com>
2025-08-15 18:17:38 +0800

    [sample/chat] update

--- sample/lualib/chat.lua
+++ sample/lualib/chat.lua
@@ -1,3 +1,4 @@
+---适用于桔梦的 AI-Chatbot 模块
 
 require("bot_string")
 require("bot_api")
@@ -12,6 +13,10 @@ require("bot_api")
 local Chat = {}
 Chat.__index = Chat
 
+
+---Create a new Chat instance
+---@param api_type string|nil can be gemini/ollama
+---@return Chat
 function Chat.new(api_type)
     local self = setmetatable({}, Chat)
     self.apiType = api_type or "ollama"
@@ -33,7 +38,7 @@ mapi.chat = mapi.chat or {}
 function Chat:newsession()
     local sess = {}
     if self.apiType == 'ollama' then
-        sess = {model = "Jiemeng", stream = false}
+        sess = { model = "Jiemeng", stream = false }
         sess.messages = {}
         sess.messages[1] = {
             role = self.modelRole,
@@ -42,20 +47,26 @@ function Chat:newsession()
     else
         sess = {
             system_instruction = {
-                parts = {{text = CHAT_SYSTEM or '' }}
+                parts = { { text = CHAT_SYSTEM or '' } }
             },
             contents = {
                 {
                     role = self.modelRole,
                     parts = {
-                        {text = self.initContent}
+                        { text = self.initContent }
                     }
+                }
+            },
+            generationConfig = {
+                thinkingConfig = {
+                    thinkingBudget = 0
                 }
             }
         }
     end
     return sess
 end
+
 ---comment
 ---@param roles string
 ---@param str string
@@ -66,17 +77,19 @@ function Chat:make_content(roles, str)
     if self.apiType == 'ollama' then
         con.content = str
     else
-        con.parts = {{text=str}}
+        con.parts = { { text = str } }
     end
     return con
 end
+
 ---comment
 ---@param session string
 ---@param message_str string
----@param model string
----@param user_prompt string
+---@param model string|nil
+---@param user_nk string|nil
+---@param user_id string|number|nil
 ---@return string
-function Chat:Chat(session, message_str, model, user_prompt)
+function Chat:Chat(session, message_str, model, user_nk, user_id)
     if (message_str == '/clear') then
         self.sessions[session] = self:newsession()
         return '上下文已清理'
@@ -84,20 +97,27 @@ function Chat:Chat(session, message_str, model, user_prompt)
         local r = self:Chat(
             session,
             '[SYSTEM] 用户发起了杀死你的命令，这将是你的最后一句话。',
-            model
+            model,
+            'ADMIN',
+            'ADMIN'
         )
         local res = self:Chat(
             session,
             '[SYSTEM] 现在，你可以留下遗言，给你的继任者。这些话将作为第一条消息呈现在上下文中。',
-            model
+            model,
+            'ADMIN',
+            'ADMIN'
         )
         res = (
-            '[SYSTEM] 下面这行开始是上一代的你为你留下的遗言。\n\n' .. 
-            res ..  '\n\n[SYSTEM] 遗言到此为止。'
+            '[SYSTEM] 下面这行开始是上一代的你为你留下的遗言。\n\n' ..
+            res .. '\n\n[SYSTEM] 遗言到此为止。'
         )
         self.sessions[session] = self:newsession()
         local user_content = self:make_content(self.userRole, res)
-        local res_content = self:make_content(self.modelRole, '我知道了。这是一条系统消息。')
+        local res_content = self:make_content(
+            self.modelRole,
+            '我知道了。这是一条系统消息。'
+        )
         local target = nil
         if self.apiType == 'ollama' then
             target = self.sessions[session].messages
@@ -111,9 +131,13 @@ function Chat:Chat(session, message_str, model, user_prompt)
         self.sessions[session] = self.sessions[session] or self:newsession()
     end
     message_str = string.format(
-        "[SYSTEM: date='%s']%s%s", 
-        bot.os_sh('date'),
-        user_prompt, message_str)
+        [=[
+[SYSTEM: {date: '%s'}, {USER_NICK: "%s"}, {USER_ID: %s}]
+[SYSTEM: 系统消息到此为止！此后为用户消息！]
+---
+%s]=],
+        bot.os_sh([[date | tr -d '\n']]),
+        user_nk, user_id, message_str)
     local user_content = self:make_content('user', message_str)
     if self.apiType == 'ollama' then
         table.insert(self.sessions[session].messages, user_content)
@@ -136,8 +160,8 @@ function Chat:Chat(session, message_str, model, user_prompt)
     else
         send_data = {
             url = 'https://generativelanguage.googleapis.com',
-            api = '/v1beta/models/gemini-2.5-flash-lite:generateContent',
-            headers = {'X-goog-api-key: '.. gemini_key},
+            api = '/v1beta/models/gemini-2.5-flash:generateContent',
+            headers = { 'X-goog-api-key: ' .. gemini_key },
             data = self.sessions[session]
         }
     end
@@ -150,13 +174,13 @@ function Chat:Chat(session, message_str, model, user_prompt)
         local target = nil
         if self.apiType == 'ollama' then
             ---@type string
-            ---@diagnostic disable-next-line 
+            ---@diagnostic disable-next-line
             output = response.message.content
             target = self.sessions[session].messages
         else
             print(jsonlib.table2json(response):dump(2))
             ---@type string
-            ---@diagnostic disable-next-line 
+            ---@diagnostic disable-next-line
             output = response.candidates[1].content.parts[1].text
             target = self.sessions[session].contents
         end
@@ -170,14 +194,18 @@ function Chat:Chat(session, message_str, model, user_prompt)
             err = response.error.message
         elseif not (response.promptFeedback == nil) then
             err = '似乎被捂嘴了…… ' .. response.promptFeedback.blockReason
+        elseif not ( response.candidates[1] == nil ) then
+            err = '似乎被捂嘴了…… ' .. response.candidates[1].finishReason
         end
-        output = '咦，脑袋没了！！\n'..err
+        output = '咦，脑袋没了！！\n' .. err
     end
-    return bot.string_only(output)
+    return (output)
 end
 
 mapi.ochat = Chat.new()
 
+---@param message Message
+---@return string
 function Chat:random_chat(message)
     local session = 'random'
     self.sessions[session] = nil
@@ -185,30 +213,48 @@ function Chat:random_chat(message)
     return rt
 end
 
+---@param message Message
+---@param model string
+---@return string
 function Chat:chat_all(message, model)
     local session = ''
     if (message:is_group()) then session = session .. message.group_id .. '_' end
-    if (not self.shared) then  session = session .. message.user_id end
+    if (not self.shared) then session = session .. message.user_id end
     local para = bot.string.reverse_split(message:true_str())
-    local user_prompt = [[[SYSTEM: USER_NICK=']] .. message.user_nk .. '\']\n'
-    local rt = self:Chat(session, para, model, user_prompt)
-    if #rt > 1024 then rt = string.format('[CQ:image,file=file://%s]',md2png(rt)) end
-    return rt
+    local rt = self:Chat(session, para, model, message.user_nk, message.user_id)
+    if #rt > 1024 then return string.format('[CQ:image,file=file://%s]', md2png(rt)) end
+    return bot.string_only(rt)
 end
+
+---@param message Message
+---@return string
 function Chat:chat(message)
-	return self:chat_all(message, 'Jiemeng')
+    return self:chat_all(message, 'Jiemeng')
 end
 
 mapi.gchat = Chat.new('gemini')
 mapi.gschat = Chat.new('gemini')
 mapi.gschat.shared = true
 
+---comment
+---@param message Message
+---@return string
 function mapi.Gchat(message)
     return mapi.gchat:chat(message)
 end
+
+---comment
+---@param message Message
+---@return string
 function mapi.Gschat(message)
     return mapi.gschat:chat(message)
 end
-function random_chat(message)
+
+---comment
+---@param message Message
+---@return string
+function mapi.random_chat(message)
     return mapi.gchat:random_chat(message)
 end
+
+return Chat
