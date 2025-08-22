ca369a4dabc1ce93ff6069b86bda06f0a6d5c360
vescrity <vescrity@foxmail.com>
2025-08-22 15:05:16 +0800

    [Core/txt2img] Fix: File should be closed before running command. update style.

--- code/txt2img_api.cpp
+++ code/txt2img_api.cpp
@@ -2,25 +2,24 @@
 #include "Jiemeng_Basic.h"
 #include <fstream>
 using string = std::string;
-string txt2img(const string &txt)
-{
-  const int N = 16;
-  static int Now = 0;
-  char name[16] = {0};
-  char txtname[32] = {0};
-  char filename[32] = {0};
-  char excmd[128] = {0};
-  sprintf(name, "text%03d", Now++);
-  sprintf(txtname, "tmp/%s.txt", name);
-  sprintf(filename, "tmp/%s.png", name);
-  sprintf(excmd, "txt2img/txt2img %s %s", txtname, filename);
-  if (Now >= N)
-    Now = 0;
+string txt2img(const string &txt) {
+    const int N = 16;
+    static int Now = 0;
+    char name[16] = {0};
+    char txtname[32] = {0};
+    char filename[32] = {0};
+    char excmd[128] = {0};
+    sprintf(name, "text%03d", Now++);
+    sprintf(txtname, "tmp/%s.txt", name);
+    sprintf(filename, "tmp/%s.png", name);
+    sprintf(excmd, "txt2img/txt2img %s %s", txtname, filename);
+    if (Now >= N)
+        Now = 0;
 
-  std::ofstream out_file(txtname);
-  if (out_file) {
-    out_file << txt;
-  }
- 
-  return execmd(excmd);
+    std::ofstream out_file(txtname);
+    if (out_file) {
+        out_file << txt;
+        out_file.close();
+    }
+    return execmd(excmd);
 }
